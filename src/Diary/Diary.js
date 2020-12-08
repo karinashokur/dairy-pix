d1d13727a793449d4eff482c5c8228cc89cfcbbd
Karina <karina.shokur@devit-team.com>
2020-12-08 14:58:24 +0000

    Initial Commit

---
+++ src/Diary/Diary.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './Diary.css';
+class Diary extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Don't Be Evil</p>
+      </div>
+    );
+  }
+}
+export default Diary;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Diary from './Diary/Diary';
+ReactDOM.render(<Diary />, document.getElementById('root'));
