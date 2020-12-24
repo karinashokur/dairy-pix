bf74f5ce0626fd2c9b48a6022ea15185d5812f9e
Karina <karina.shokur@devit-team.com>
2020-12-24 13:28:29 +0000

    Added `classnames` package to handle conjunction of class names more conveniently

--- src/Day/Day.js
+++ src/Day/Day.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import './Day.css';
+import classNames from 'classnames';
 class Day extends Component {
   render() {
-    let className = 'day';
-    let onclick = this.props.onClick;
-    if(this.props.filler) {
-      className += ' filler';
-      onclick = null;
-    }
-    if(this.props.data.color) {
-      className += ' colored';
-    }
-    return <div className={className} onClick={onclick}></div>;
+    const classes = classNames({
+      'day': true,
+      'filler': this.props.filler,
+      'colored': this.props.data.color
+    });
+    return <div className={classes} onClick={!this.props.filler ? this.props.onClick : null}></div>;
   }
 }
 export default Day;
