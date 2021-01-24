3c7d124f6aa1dcb6a143755f010128004591ea1f
Karina <karina.shokur@devit-team.com>
2021-01-24 21:03:24 +0000

    Fixed test

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import gitlabLogo from '../assets/gitlab.svg';
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App name="Test App" />, div);
+  ReactDOM.render(
+    <App
+      name="Test App"
+      repository={{
+        name: 'GitLab',
+        url: 'https:
+        logoSrc: gitlabLogo,
+      }}
+    />,
+    div,
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
