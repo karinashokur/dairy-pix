31cb2a3ccd04fc28d3d30a3839f2ddbb209b146a
Karina <karina.shokur@devit-team.com>
2021-04-26 04:01:14 +0000

    Added a note regarding Bugsnag to the privacy information

--- src/privacy.tsx
+++ src/privacy.tsx
@@ -2,23 +2,37 @@ import React, { Fragment } from 'react';
 const privacyInfo = (
   <Fragment>
     <p>
-      This website does not use cookies and does not collect any kind of information about its
-      visitors. All information entered on this website, for example a mood or a note (hereafter
+      This website does not use cookies and does not collect any kind of personal information about
+      its visitors. All information entered on this website, for example a mood or a note (hereafter
       called "Diary Content"), are stored in the integrated storage of the visitors browser, unless
       the visitor chooses to connect a cloud storage (see "Cloud Storage").
     </p>
+    <h4>Information Collected for Error Reporting</h4>
+    <p>
+      This website uses <a href="https:
+      error reports. These error reports include basic non-personally-identifying information from
+      website visitors of the sort that web browsers typically make available, such as the browser
+      type and version, name of the operating system, language preference, and the date and time the
+      error occurred.<br />
+      <br />
+      However, Bugsnag might collect additional information when connecting to their servers. Details
+      can be found in their Privacy Policy at <a href="https:
+      docs.bugsnag.com/legal/privacy-policy</a>.
+    </p>
     <h4>Information Collected by the Hoster</h4>
     <p>
-      This website is hosted using a service of GitLab B.V. ("GitLab"). Like most hosters, GitLab
-      collects basic non-personally-identifying information from website visitors of the sort that
-      web browsers and servers typically make available, such as the browser type, language
-      preference, referring site, and the date and time of each visitor request.<br />
+      This website is hosted using a service of <a href="https:
+      GitLab B.V.</a> ("GitLab"). Like most hosters, GitLab collects basic non-personally-identifying
+      information from website visitors of the sort that web browsers and servers typically make
+      available, such as the browser type, language preference, referring site, and the date and
+      time of each visitor request.<br />
       <br />
       GitLab also collects potentially personally-identifying information like Internet Protocol
       (IP) addresses from visitors. GitLab does not use such information to identify or track
       individual visitors, however.<br />
       <br />
       Further details can be found in GitLabs Privacy Policy at <a href="https:
+      about.gitlab.com/privacy</a>.
     </p>
     <h4>Cloud Storage</h4>
     <p>
